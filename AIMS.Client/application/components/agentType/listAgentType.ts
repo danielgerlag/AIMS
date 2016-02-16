3a10ae1d2210ea75ab2b2e0d6175be74e16295f0
daniel <dgerlag@westlandinsurance.ca>
2016-02-16 10:24:50 -0800

    .

---
+++ AIMS.Client/application/components/agentType/listAgentType.ts
@@ -0,0 +1,35 @@
+﻿import {FormBuilder, Validators, ControlGroup, Control, NgClass, FORM_BINDINGS, CORE_DIRECTIVES, FORM_DIRECTIVES, JsonPipe} from 'angular2/common';
+import {Router} from 'angular2/router';
+
+import {Component, View} from 'angular2/core';
+
+import {IShellService} from '../../services/shellService';
+import {IAuthService} from '../../services/authService';
+import {IDataService} from '../../services/dataService';
+import {ListController} from '../../core/listController';
+
+@Component({
+    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass],
+    templateUrl: './application/core/listController.html'
+})
+export class ListAgentType extends ListController {
+
+    constructor(shellService: IShellService, dataService: IDataService, router: Router) {
+        super(shellService, dataService, router);        
+    }
+    
+    protected setName(): string {
+        return "AgentTypes";
+    }
+
+    protected itemRoute(): string {
+        return "EditAgentType";
+    }
+
+    protected getColumns(): any {
+        return [
+            { Title: 'Name', Name: 'Name' }
+        ];
+    }
+    
+}
---
+++ AIMS.Client/application/components/serviceProviderType/editServiceProviderType.ts
@@ -0,0 +1,42 @@
+﻿import {ROUTER_DIRECTIVES, RouteConfig, Location, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy, Route, AsyncRoute, Router} from 'angular2/router';
+import {FormBuilder, Validators, ControlGroup, Control, NgClass, FORM_BINDINGS, CORE_DIRECTIVES, FORM_DIRECTIVES, JsonPipe} from 'angular2/common';
+import {RouteParams} from 'angular2/router';
+import {TAB_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
+import {Component, View} from 'angular2/core';
+import {EntityDropdown} from '../../directives/input/entityDropdown';
+import {FormInput} from '../../directives/input/formInput';
+import {EntitySummary} from '../../directives/input/entitySummary';
+
+import {IShellService} from '../../services/shellService';
+import {IAuthService} from '../../services/authService';
+import {IDataService} from '../../services/dataService';
+import {CRUDController} from '../../core/crudController';
+
+@Component({    
+    templateUrl: './application/components/serviceProviderType/editServiceProviderType.html',
+    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, FormInput, NgClass, EntitySummary, EntityDropdown, TAB_DIRECTIVES],
+    pipes: [JsonPipe]
+})
+export class EditServiceProviderType extends CRUDController {
+
+    constructor(params: RouteParams, router: Router, location: Location, dataService: IDataService, shellService: IShellService, authService: IAuthService, fb: FormBuilder) {
+        super(params, router, location, dataService, shellService, authService, fb);
+        this.title = "Service Provider Type";
+    }
+
+    protected typeName(): string {
+        return "ServiceProviderType";
+    }
+
+    protected setName(): string {
+        return "ServiceProviderTypes";
+    }
+
+
+    protected afterSave(sender: EditServiceProviderType, data: any) {
+        super.afterSave(sender, data);
+        sender.router.navigate(["Home"]);
+    }
+
+   
+}
